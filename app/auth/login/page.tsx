262791db3141b8138bccefbcdd779d7a66fa97a5
Apriyanto Dwi Herlambang <dwiherlambangapri@gmail.com>
2025-09-17 11:14:50 +0700

    feat(auth): update login and forgot password forms + responsive

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,24 +1,19 @@
-import { LoginForm } from "@/components/desktop/login-form";
-import Image from "next/image";
+"use client";
 
-export default function Page() {
+import { DesktopLoginForm } from "@/components/desktop/login-form";
+import { MobileLoginForm } from "@/components/mobile/login-form";
+
+export default function LoginPage() {
   return (
-    <div className="grid min-h-screen w-full grid-cols-1 md:grid-cols-2">
-      <div className="relative flex flex-col items-center justify-center p-6 text-white">
-        <Image
-          src="/bg_alfamidi2.svg"
-          alt="Alfamidi Background"
-          fill
-          className="object-cover"
-          priority
-        />
-        <div className="absolute inset-0 bg-red-800 opacity-40"></div>
-        <div className="relative z-10 text-center"></div>
+    <div className="min-h-screen">
+      {/* Mobile & Tablet */}
+      <div className="block lg:hidden">
+        <MobileLoginForm />
       </div>
-      <div className="flex items-center justify-center bg-gray-100 p-6">
-        <div className="w-full max-w-sm">
-          <LoginForm />
-        </div>
+
+      {/* Desktop */}
+      <div className="hidden lg:block">
+        <DesktopLoginForm />
       </div>
     </div>
   );
